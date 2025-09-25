e1cead99ea077a7a970b0d5fd9c5264498a0c3ee
fef-on-berg <157720148+fef-on-berg@users.noreply.github.com>
2025-09-25 16:32:34 +0000

    Füge Unterstützung für das Überspringen des Wartens im Service Worker hinzu und entferne die To-Do-Liste

--- docs/serviceworker.js
+++ docs/serviceworker.js
@@ -116,3 +116,9 @@ function trimCache(cacheName, maxEntries) {
     })
   );
 }
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
